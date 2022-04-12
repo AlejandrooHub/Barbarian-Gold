62c652757986e8e58771956b3d9590da498a2f6f
al394503 <al394503@uji.es>
2022-04-12 18:53:31 +0200

    Maze added not working offset

--- app/src/main/java/es/uji/al394503/barbariangold/view/MainActivity.kt
+++ app/src/main/java/es/uji/al394503/barbariangold/view/MainActivity.kt
@@ -3,8 +3,8 @@ package es.uji.al394503.barbariangold.view
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.os.Bundle
-import es.uji.al394503.barbariangold.R
 import es.uji.al394503.barbariangold.controller.Controller
+import es.uji.al394503.barbariangold.model.CellType
 import es.uji.al394503.barbariangold.model.Model
 import es.uji.vj1229.framework.GameActivity
 import es.uji.vj1229.framework.Graphics
@@ -25,8 +25,32 @@ class MainActivity : GameActivity() {
     }
 
     override fun onDrawingRequested(): Bitmap {
+        val size:Float = ((graphics.width/model.maze.nCols)/1.5f) + 1
+/*
+        val widthOffset = (model.maze.nCols.toFloat()/2f) * size + graphics.width/2
+        val heightOffset = (model.maze.nRows.toFloat()/4f) * size + graphics.height/2
+*/
+
+        val widthOffset = graphics.width/2 - model.maze.nCols/2 * size
+        val heightOffset = size
+
+
+
+
         graphics.clear(Color.BLUE)
-        graphics.drawRect(0f,0f, 10f,10f, Color.WHITE)
+        for (i in 0 until model.maze.nRows)
+            for (j in 0 until model.maze.nCols){
+                when(model.maze.get(i, j).type) {
+                    CellType.ORIGIN -> graphics.drawRect(j.toFloat()*size + widthOffset,i.toFloat()*size + heightOffset, size,size, Color.WHITE)
+                    CellType.POTION -> graphics.drawRect(j.toFloat()*size + widthOffset,i.toFloat()*size + heightOffset, size,size, Color.BLUE)
+                    CellType.HOME -> graphics.drawRect(j.toFloat()*size + widthOffset,i.toFloat()*size + heightOffset, size,size, Color.RED)
+                    CellType.DOOR -> graphics.drawRect(j.toFloat()*size + widthOffset,i.toFloat()*size + heightOffset, size,size, Color.MAGENTA)
+                    CellType.GOLD -> graphics.drawRect(j.toFloat()*size + widthOffset,i.toFloat()*size + heightOffset, size,size, Color.YELLOW)
+                    CellType.WALL -> graphics.drawRect(j.toFloat()*size + widthOffset,i.toFloat()*size + heightOffset, size+1,size+1, Color.LTGRAY)
+                    else -> graphics.drawRect(j.toFloat()*size + widthOffset,i.toFloat()*size + heightOffset, size,size, Color.TRANSPARENT)
+                }
+            }
+
         return graphics.frameBuffer
     }
 
